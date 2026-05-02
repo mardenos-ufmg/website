0ea903b98fb6655f2911c8adf3ef35eaf54c1b01
Carlos de Moura <lcbarrosdemoura@gmail.com>
2026-05-02 08:55:41 -0300

    conf conditional os

---
+++ conf/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+source /etc/os-release
+
+HOME_DIR=$(eval echo "~$SUDO_USER")
+
+quarto render "$HOME_DIR/website"
+
+rm -rf /var/www/html/*
+cp -r "$HOME_DIR/website/_site/." /var/www/html
+
+rm -rf /var/www/autoindex
+mkdir -p /var/www/autoindex
+cp -r "$HOME_DIR/website/autoindex/." /var/www/autoindex
+
+
+if [[ "$ID" == "ubuntu" ]]; then
+    cp "$HOME_DIR/website/conf/nginx.conf" /etc/nginx/sites-available/default
+
+elif [[ "$ID" == "fedora" ]]; then
+    cp "$HOME_DIR/website/conf/nginx.conf" /etc/nginx/conf.d/default.conf
+
+else
+    echo "Distribuição não suportada: $ID"
+    exit 1
+fi
+
+systemctl restart nginx
+
+
+pkill -f shiny-server || true
+shiny-server "$HOME_DIR/website/conf/shiny-server.conf"
